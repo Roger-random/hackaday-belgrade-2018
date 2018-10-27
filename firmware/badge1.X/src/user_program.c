34d31497f5f79126da74c8e4cbfc42a0dea3d5ef
Roger Cheng <roger.random@outlook.com>
2018-10-27 16:32:27 -0700

    Tabs to spaces

--- firmware/badge1.X/src/user_program.c
+++ firmware/badge1.X/src/user_program.c
@@ -62,160 +62,159 @@ uint32_t play_next_note(uint8_t measure_index, uint8_t note_index, uint8_t *next
 }
 
 void user_program_init(void)
-	{
-	K_R1 = 1;
-	K_R2 = 1;
-	K_R3 = 1;
-	K_R4 = 1;
-	K_R5 = 1;
-    }
+{
+    K_R1 = 1;
+    K_R2 = 1;
+    K_R3 = 1;
+    K_R4 = 1;
+    K_R5 = 1;
+}
 
 void user_program_loop(void)
-	{
-        enable_display_scanning(0); //Shut off auto-scanning of character buffer
-        uint16_t x = 0;
-        uint8_t y = 0;
+{
+    enable_display_scanning(0); //Shut off auto-scanning of character buffer
+    uint16_t x = 0;
+    uint8_t y = 0;
 
-        // 320 pixels wide line, each 32-bit value in 0xNNRRGGBB format where
-        // NN = Not used, RR = 8-bit red, GG = 8-bit green, BB = 8-bit blue
-        uint32_t line[320];
+    // 320 pixels wide line, each 32-bit value in 0xNNRRGGBB format where
+    // NN = Not used, RR = 8-bit red, GG = 8-bit green, BB = 8-bit blue
+    uint32_t line[320];
 
-        uint16_t i = 0;
-        uint32_t pixel_idx = 0;
-        uint16_t encoded_run = 0;
-        uint32_t pal_entry = 0;
-        uint16_t pal_run = 0;
-        uint8_t frame_idx = 0;
+    uint16_t i = 0;
+    uint32_t pixel_idx = 0;
+    uint16_t encoded_run = 0;
+    uint32_t pal_entry = 0;
+    uint16_t pal_run = 0;
+    uint8_t frame_idx = 0;
 
-        // Image scaling control. 1=full scale, 2=half scale, 4=quarter, etc.
-        uint8_t multiplier = 1;
-        uint8_t multiply_loop=0;
+    // Image scaling control. 1=full scale, 2=half scale, 4=quarter, etc.
+    uint8_t multiplier = 1;
+    uint8_t multiply_loop=0;
 
-        // Animation speed control
-        const uint8_t milliseconds_per_frame = 70;
-        uint32_t time_for_next_frame = millis();
+    // Animation speed control
+    const uint8_t milliseconds_per_frame = 70;
+    uint32_t time_for_next_frame = millis();
 
-        // Audio playback control
-        // audio_state is the state machine that sequences measures into a song.
-        // Each measure has zero or more 'notes', different from sheet music notes.
-        // Each note is a set of three frequencies to be held fof a specific time.
-        uint32_t time_for_audio_update = millis();
-        uint8_t audio_state = 0;
-        uint8_t next_audio_state = 0;
-        uint8_t measure_index = 0;
-        uint8_t note_index = 0;
-        uint8_t next_note = 0;
+    // Audio playback control
+    // audio_state is the state machine that sequences measures into a song.
+    // Each measure has zero or more 'notes', different from sheet music notes.
+    // Each note is a set of three frequencies to be held fof a specific time.
+    uint32_t time_for_audio_update = millis();
+    uint8_t audio_state = 0;
+    uint8_t next_audio_state = 0;
+    uint8_t measure_index = 0;
+    uint8_t note_index = 0;
+    uint8_t next_note = 0;
 
-        while(1) //Loop forever
+    while(1) //Loop forever
+    {
+        // Is it time for the next frame?
+        if (millis() >= time_for_next_frame)
         {
-            // Is it time for the next frame?
-            if (millis() >= time_for_next_frame)
+            time_for_next_frame += milliseconds_per_frame;
+
+            pixel_idx=0;
+            if(frame_idx > 11)
             {
-                time_for_next_frame += milliseconds_per_frame;
-                
-                pixel_idx=0;
-                if(frame_idx > 11)
-                {
-                    frame_idx = 0;
-                }
+                frame_idx = 0;
+            }
 
-                for (y=0;y<240;y+=multiplier)
-                {
-                    x = 0;
+            for (y=0;y<240;y+=multiplier)
+            {
+                x = 0;
 
-                    while (x < 320)
+                while (x < 320)
+                {
+                    switch(multiplier)
                     {
-                        switch(multiplier)
-                        {
-                            case 2:
-                                encoded_run = cat2_frames[frame_idx][pixel_idx];
-                                pal_entry = cat2_palette[(encoded_run>>12)&0xF];
-                                break;
-                            case 4:
-                                encoded_run = cat4_frames[frame_idx][pixel_idx];
-                                pal_entry = cat4_palette[(encoded_run>>12)&0xF];
-                                break;
-                            case 8:
-                                encoded_run = cat8_frames[frame_idx][pixel_idx];
-                                pal_entry = cat8_palette[(encoded_run>>12)&0xF];
-                                break;
-                            default:
-                                encoded_run = cat1_frames[frame_idx][pixel_idx];
-                                pal_entry = cat1_palette[(encoded_run>>12)&0xF];
-                        }
-                        pal_run = encoded_run&0xFFF;
-                        for (i = 0; i < pal_run; i++)
-                        {
-                            for (multiply_loop=0; multiply_loop<multiplier; multiply_loop++)
-                            {
-                                line[x+i*multiplier+multiply_loop] = pal_entry;
-                            }
-                        }
-                        x += pal_run*multiplier;
-                        pixel_idx++;
+                        case 2:
+                            encoded_run = cat2_frames[frame_idx][pixel_idx];
+                            pal_entry = cat2_palette[(encoded_run>>12)&0xF];
+                            break;
+                        case 4:
+                            encoded_run = cat4_frames[frame_idx][pixel_idx];
+                            pal_entry = cat4_palette[(encoded_run>>12)&0xF];
+                            break;
+                        case 8:
+                            encoded_run = cat8_frames[frame_idx][pixel_idx];
+                            pal_entry = cat8_palette[(encoded_run>>12)&0xF];
+                            break;
+                        default:
+                            encoded_run = cat1_frames[frame_idx][pixel_idx];
+                            pal_entry = cat1_palette[(encoded_run>>12)&0xF];
                     }
-
-                    tft_set_write_area(0,y,319,multiplier);
-                    TFT_24_7789_Write_Command(0x2C);
-                    for (multiply_loop=0; multiply_loop<multiplier; multiply_loop++)
+                    pal_run = encoded_run&0xFFF;
+                    for (i = 0; i < pal_run; i++)
                     {
-                        for (i=0;i<320;i++)
+                        for (multiply_loop=0; multiply_loop<multiplier; multiply_loop++)
                         {
-                            pal_entry = line[i];
-                            TFT_24_7789_Write_Data3((pal_entry>>16)&0xFF,(pal_entry>>8)&0xFF,(pal_entry>>0)&0xFF);
+                            line[x+i*multiplier+multiply_loop] = pal_entry;
                         }
                     }
+                    x += pal_run*multiplier;
+                    pixel_idx++;
                 }
-                frame_idx++;
-            }
 
-            // Is it time for the next audio update?
-            if (millis() >= time_for_audio_update)
-            {
-                if (audio_state != 0xFF)
+                tft_set_write_area(0,y,319,multiplier);
+                TFT_24_7789_Write_Command(0x2C);
+                for (multiply_loop=0; multiply_loop<multiplier; multiply_loop++)
                 {
-                    measure_index = mario_state_measures[audio_state];
-                    next_audio_state = mario_state_next[audio_state];
-
-                    time_for_audio_update += play_next_note(measure_index,note_index,&next_note);
-                    if (next_note==0)
+                    for (i=0;i<320;i++)
                     {
-                        audio_state = next_audio_state;
+                        pal_entry = line[i];
+                        TFT_24_7789_Write_Data3((pal_entry>>16)&0xFF,(pal_entry>>8)&0xFF,(pal_entry>>0)&0xFF);
                     }
-                    note_index = next_note;
-                }
-                else
-                {
-                    // Fallback - silence for 10 seconds.
-                    sound_set_note(0,0);
-                    sound_set_note(0,1);
-                    sound_set_note(0,2);
-                    time_for_audio_update += 10000;
                 }
             }
-
-            // Check for specific keys that change our behavior.
-            K_R1 = 0;
-            
-            if (K_C10==0) // '8' key is down
-            {
-                multiplier=8;
-            }
-            else if (K_C2==0) // '4' key is down
-            {
-                multiplier=4;
-            }
-            else if (K_C3==0) // '2' key is down
-            {
-                multiplier=2;
-            }
-            else if (K_C5==0) // '1' key is down
-            {
-                multiplier=1;
-            }
-            
-            K_R1 = 1;
+            frame_idx++;
         }
-        //Badge will need to be reset to continue
-	}
 
+        // Is it time for the next audio update?
+        if (millis() >= time_for_audio_update)
+        {
+            if (audio_state != 0xFF)
+            {
+                measure_index = mario_state_measures[audio_state];
+                next_audio_state = mario_state_next[audio_state];
+
+                time_for_audio_update += play_next_note(measure_index,note_index,&next_note);
+                if (next_note==0)
+                {
+                    audio_state = next_audio_state;
+                }
+                note_index = next_note;
+            }
+            else
+            {
+                // Fallback - silence for 10 seconds.
+                sound_set_note(0,0);
+                sound_set_note(0,1);
+                sound_set_note(0,2);
+                time_for_audio_update += 10000;
+            }
+        }
+
+        // Check for specific keys that change our behavior.
+        K_R1 = 0;
+
+        if (K_C10==0) // '8' key is down
+        {
+            multiplier=8;
+        }
+        else if (K_C2==0) // '4' key is down
+        {
+            multiplier=4;
+        }
+        else if (K_C3==0) // '2' key is down
+        {
+            multiplier=2;
+        }
+        else if (K_C5==0) // '1' key is down
+        {
+            multiplier=1;
+        }
+
+        K_R1 = 1;
+    }
+    //Badge will need to be reset to continue
+}
